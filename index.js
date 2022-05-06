8c92c1e4e03a96bee73da722e137fd16c7807e22
sagor devnath <ssagor64@gmail.com>
2022-05-06 15:04:49 +0600

    get inventory add

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -9,6 +10,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k7yl5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -17,6 +34,16 @@ async function run() {
     try{
         await client.connect();
         const toysCollection = client.db('nathToys').collection('inventory');
+        const MyCollection = client.db('nathToys').collection('myInventory');
+
+        //AUTH
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
 
         // get all data from mongodb
         app.get('/inventory', async(req, res) => {
@@ -68,10 +95,25 @@ async function run() {
         })
 
         // POST single data
-        app.post('/inventory', async(req, res)=> {            
-            const newInventory = req.body;
-            const result = await toysCollection.insertOne(newInventory);
+        app.post('/myInventory', async(req, res)=> {            
+            const myInventory = req.body;            
+            const result = await MyCollection.insertOne(myInventory);
             res.send(result);
+        })
+
+        // get my items data
+        app.get('/MyInventory', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = MyCollection.find(query);
+                const myInventory = await cursor.toArray();
+                res.send(myInventory);
+            }
+            else{
+                res.status(403).send({message: 'forbidden access'})
+            }
         })
 
         
